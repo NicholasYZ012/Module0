3d44e69587babd5b3da33b12d3922c592544757a
NicholasYZ <nicholasyz012@gmail.com>
2026-08-12 17:30:35 +0800

    fix strlcpy error

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: nilim <nilim@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 23:58:06 by nilim             #+#    #+#             */
-/*   Updated: 2026/08/11 10:32:25 by nilim            ###   ########.fr       */
+/*   Updated: 2026/08/12 17:30:25 by nilim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	i = 0;
 	if (!size)
 		return (ft_strlen(src));
-	while (i < size - 1 && src)
+	while (i < size - 1 && src[i])
 	{
 		dst[i] = src[i];
 		i++;
